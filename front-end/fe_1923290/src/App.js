77b46764b4a5f46b7771f3b60ab0471a6a4c0c1c
IainPeddie <1923290@brunel.ac.uk>
2024-07-30 22:37:41 +0100

    Added Progress Bar and Header
    Added the progress bar and header, had issues with getting AppRouter setup correctly, didn't realise I had to refer to it in the index.html file

--- front-end/fe_1923290/src/App.js
+++ front-end/fe_1923290/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Link, Outlet} from "react-router-dom";
+import React from "react";
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <nav className="App-progress"> 
+        <Link className="nav-link" to={'/'}>Input</Link>
+        <Link className="nav-link" to={'/expenses'}>Expenses</Link>
+        <Link className="nav-link" to={'/list'}>List</Link>
+        <Link className="nav-link" to={'/result'}>Result</Link> 
+      </nav>
+      <main classname="App-main">
+      <Outlet/>
+      </main>
+      
+
     </div>
   );
 }
