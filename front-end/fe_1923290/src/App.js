59f788902c78789873884e921a9514fc41b3c0c1
IainPeddie <1923290@brunel.ac.uk>
2024-07-31 02:42:42 +0100

    Income Input and Expense Input
    Added Income Input and Expenses Input to the app, Income is finished, but expenses needs tables to be added.

--- front-end/fe_1923290/src/App.js
+++ front-end/fe_1923290/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import {Link, Outlet} from "react-router-dom";
 import React from "react";
 import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <nav className="App-progress"> 
-        <Link className="nav-link" to={'/'}>Input</Link>
+        <Link className="nav-link" to={'/'}>Income</Link>
         <Link className="nav-link" to={'/expenses'}>Expenses</Link>
         <Link className="nav-link" to={'/list'}>List</Link>
         <Link className="nav-link" to={'/result'}>Result</Link> 
@@ -16,8 +17,8 @@ function App() {
       <main classname="App-main">
       <Outlet/>
       </main>
+      <Footer/>
       
-
     </div>
   );
 }
